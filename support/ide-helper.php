fe431a6f364c758436e8989da424c77cacad49e2
Pozitronik <antikillerxxl@gmail.com>
2022-05-30 16:34:08 +0300

    ide helper

---
+++ support/ide-helper.php
@@ -0,0 +1,28 @@
+<?php /** @noinspection EmptyClassInspection */
+declare(strict_types = 1);
+
+use yii\BaseYii;
+use yii\queue\Queue;
+
+/**
+ * Yii bootstrap file.
+ * Used for enhanced IDE code autocompletion.
+ * Note: To avoid "Multiple Implementations" PHPStorm warning and make autocomplete faster
+ * exclude or "Mark as Plain Text" vendor/yiisoft/yii2/Yii.php file
+ */
+class Yii extends BaseYii {
+	/**
+	 * @var BaseApplication the application instance
+	 */
+	public static $app;
+}
+
+/**
+ * Class BaseApplication
+ * Used for properties that are identical for both WebApplication and ConsoleApplication
+ *
+ * @property Queue $queue
+ */
+abstract class BaseApplication extends yii\base\Application {
+
+}
